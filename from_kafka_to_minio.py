12158bc91bd0ec1e7091df476c69e0d50fdf84b7
Aleksandr Shitikov <aleksandr_shitikov@epam.com>
2021-05-27 19:31:53 +1000

    add jars

--- from_kafka_to_minio.py
+++ from_kafka_to_minio.py
@@ -1,26 +1,13 @@
+#--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1,org.apache.spark:spark-streaming-kafka-0-10_
 from os.path import expanduser, join, abspath
 from pyspark.sql import SparkSession
-spark = SparkSession \
-    .builder \
-    .appName("stage_1") \
-    .getOrCreate()
+spark = SparkSession.builder.config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1").appName("stage_1").getOrCreate()
 
 print("Creating spark df")
 
-df = spark \
-  .readStream \
-  .format("kafka") \
-  .option("kafka.bootstrap.servers", "kafka-cluster-kafka-bootstrap.ddt-persistence.svc.cluster.local:9092") \
-  .option("subscribe", "ddt") \
-  .option("startingOffsets", "earliest") \
-  .load()
-
+df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "kafka-cluster-kafka-bootstrap.ddt-persistence.svc.cluster.local:9092").option("subscribe", "ddt").option("startingOffsets", "earliest").load()
 def console_output(df, freq):
-    return df.writeStream \
-        .format("console") \
-        .trigger(processingTime='%s seconds' % freq ) \
-        .options(truncate=True) \
-        .start()
+    return df.writeStream.format("console").trigger(processingTime='%s seconds' % freq ).options(truncate=True).start()
 
 if  df.isStreaming:
     print("We are streaming!")
