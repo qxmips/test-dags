a53b33f522cfef848b9adc116412c9b6689f7a4b
Aleksandr Shitikov <aleksandr_shitikov@epam.com>
2021-06-04 15:22:40 +1000

    dedicated directory for currated area

--- parquet_enrichment.py
+++ parquet_enrichment.py
@@ -2,8 +2,10 @@ from os.path import expanduser, join, abspath
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, to_timestamp, col, expr
 from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.functions import spark_partition_id, asc, desc, current_timestamp
 import time
-
+import time
+import datetime
 spark = SparkSession.builder.config("spark.jars.ivy", "/tmp").appName("stage_1").getOrCreate()
 hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
 hadoop_conf.set("fs.s3a.access.key", "minio")
@@ -16,6 +18,7 @@ checkpoint_path="s3a://spark/checkpoint"
 
 #   should we use streaming or raw
 df = spark.read.load("s3a://spark/output.parquet")
-df.show(10,False)
-df.write.mode("append").partitionBy("well_id").format("parquet").save("s3a://spark/output2.parquet")
+df_enriched = df.withColumn("partition_id", spark_partition_id()).withColumn("timestamp",current_timestamp()) 
+df_enriched.show(10,False)
+df_enriched.write.mode("append").partitionBy("well_id").format("parquet").save("s3a://spark/curated/output.parquet")
 spark.stop()
