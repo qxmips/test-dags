84c140b6ccd97ee26635ece971ade9fd52e70cb8
Aleksandr Shitikov <aleksandr_shitikov@epam.com>
2021-06-21 17:02:38 +1000

    fix

--- parquet_enrichment.py
+++ parquet_enrichment.py
@@ -1,8 +1,9 @@
 from os.path import expanduser, join, abspath
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, to_timestamp, col, expr, lit
+from pyspark.sql.functions import from_json, to_timestamp, col, expr
 from pyspark.sql.types import StructType, StructField, StringType
 from pyspark.sql.functions import spark_partition_id, asc, desc, current_timestamp
+import time
 import time
 import datetime
 spark = SparkSession.builder.config("spark.jars.ivy", "/tmp").appName("stage_1").getOrCreate()
@@ -15,11 +16,12 @@ hadoop_conf.set("fs.s3a.path.style.access", "true")
 output_path = "s3a://spark/output.parquet"
 checkpoint_path="s3a://spark/checkpoint"
 
-#   should we use streaming or raw
+#   should we use streaming or raw
 df = spark.read.load("s3a://spark/output.parquet")
 df_enriched = df.withColumn("partition_id", spark_partition_id()).withColumn("timestamp",current_timestamp()) 
 df_enriched.show(10,False)
 df_enriched.write.mode("overwrite").format("parquet").save("s3a://spark/curated/output.parquet")
+spark.stop()
 
 #m7  Spark should report partition row count on each landing to raw transformation, both read and written
 # countByPartitionRead = df.groupBy(spark_partition_id()).count().withColumn("@timestamp", current_timestamp().cast("String")).withColumn("stage",  lit("read"))
@@ -31,5 +33,3 @@ df_enriched.write.mode("overwrite").format("parquet").save("s3a://spark/curated/
 # for i in sorted(countByPartitionRead, key=lambda x: x[1]):
 #         print(i)
 # print('*' * 50)
-
-spark.stop()
